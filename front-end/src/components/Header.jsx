94888cfc9736e6df922ec61439cc28aece6e8d0d
NikolasPires <nikolasspires@gmail.com>
2024-09-08 19:42:44 -0300

    interações homepage e seção um pouco sobre nós

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,9 +1,28 @@
 import styles from './Header.module.css'
 import logo from '../assets/images/portalrj-logo.png'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 export default function Header() {
+    const [opacity, setOpacity] = useState(1);
+
+    const handleScroll = () => {
+        // Calcula a nova opacidade baseado na posição do scroll
+        const newOpacity = Math.max(0.9, 1 - window.scrollY / 300);
+        setOpacity(newOpacity);
+      };
+   
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        // Remove o event listener no componente desmontar
+        return () => {
+        window.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
+
     return (
-        <header className={styles.header}>
+        <header className={styles.header} onScroll={handleScroll} style={ {opacity } }>
             <div><Link><img src={logo} alt="" srcset="" /></Link></div>
             <ul>
                 <li><Link>Home</Link></li>
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,4 +1,9 @@
 import styles from './Home.module.css'
+import elementoVazado from '../assets/images/portal-rj-elemento-amarelo.jpg'
+import elementoMadeira from '../assets/images/portalrj-elementovazado-marrom.jpg'
+
+import { GiBrickWall } from "react-icons/gi";
+
 
 export default function Home() {
     return (
@@ -7,7 +12,26 @@ export default function Home() {
                 <h1>A arte em concreto em um só Lugar</h1>
             </section>
             <section className={styles.quemsomos}>
-                <h2>Um pouco sobre nós</h2>
+                <div>
+                    <h2>Um pouco sobre nós</h2>
+                </div>
+                <article>
+                    <div className={styles.images}>
+                        <img src={elementoVazado} alt="" />
+                        <div className={styles.overlay}><img src={elementoMadeira} alt="" /></div>
+                    </div>
+                    <div className={styles.text}>
+                        <h3>Somos a maior fabricante de decorativos em <span>concreto</span> do <span>Vale Paraíba</span> e região!</h3>
+                        <p>Bem-vindo ao <span>PortalRJ</span>, onde a beleza e a inovação se encontram no mundo do concreto. Com um compromisso inabalável com a qualidade e um olhar atento às <span>tendências</span> contemporâneas, somos especialistas em <span>transformar</span> o concreto em verdadeiras obras de arte que elevam qualquer ambiente</p>
+                        <div className={styles.slogan}>
+                            <GiBrickWall className={styles.brick} size={140}/>
+                            <p>Já são dezenas de municípios afetados pela simplicidade e autenticidade de nossos decorativos em concreto</p>
+                        </div>
+                    </div>
+                </article>
+            </section>
+            <section className={styles.produtos}>
+
             </section>
         </section>
     )
